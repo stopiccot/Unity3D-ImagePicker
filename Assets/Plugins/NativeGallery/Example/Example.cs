89d08cdbe8e157eae073cd265be657f59f367257
stopiccot <alexey.petruchik@gmail.com>
2016-12-02 18:54:23 +0300

    ololo

---
+++ Assets/Plugins/NativeGallery/Example/Example.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour {
+
+	public async void TakePhoto() {
+		var path = await Stopiccot.NativeGallery.TakePhoto();
+		Debug.Log(path);
+	}
+
+	public async void SelectPhoto() {
+		var path = await Stopiccot.NativeGallery.SelectPhoto();
+		Debug.Log(path);
+	}
+}
